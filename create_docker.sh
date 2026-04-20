7725b42e12904acb887b1cd82f82d79b6268fd6f
Your Name <you@example.com>
2026-04-20 04:59:27 +0000

    update docker creation script

--- create_docker.sh
+++ create_docker.sh
@@ -36,4 +36,5 @@ docker run -it \
   --group-add rdma \
   -v /:/dockerx \
   -v /dev/infiniband:/dev/infiniband \
+  -v /usr/lib/x86_64-linux-gnu/libibverbs/libionic-rdmav34.so:/usr/lib/x86_64-linux-gnu/libibverbs/libionic-rdmav34.so:ro \
   ${IMAGE}
